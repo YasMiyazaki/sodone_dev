51ac3870e6cc4f498119a5e119f86c57509fd374
YasMiyazaki <kizayami.yasi@gmail.com>
2018-09-04 03:17:08 +0000

    omniauth and toppage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # send email when signup
+  config.action_mailer.default_url_options
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true # changed false to true on 20180904
+
 
   config.action_mailer.perform_caching = false
 
